fc739ed2a9aeb8d99a2074be418d76f819c2543f
pkn <PKN199@yandex.ru>
2023-09-16 11:52:36 +0300

    Home work for lesson 1

---
+++ HomeWokkForLesson1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
+a = 5; b = 7 -> max = 7 
+a = 2 b = 10 -> max = 10 
+a = -9 b = -3 -> max = -3 
+*/
+
+int a = new Random().Next(1, 10); 
+int b = new Random().Next(1, 10);
+Console.Write("Число a = ");
+Console.Write(a);
+Console.Write(", число b = ");
+Console.WriteLine(b);
+
+int max = 0;
+if (a>b)
+{
+    max = a;
+}else{
+max =b;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ HomeWokkForLesson1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 
+2, 3, 7 -> 7 
+44 5 78 -> 78 
+22 3 9 -> 22 
+*/
+
+int[] array = new int[3];
+int length = 3;
+int index = 0;
+int i = 0;
+int maxArray = 0;
+while (index < length) 
+{
+    array[index] = new Random().Next(1, 100);
+    index +=1;
+}
+// array[1] = 120;
+while (i < length)
+{
+    if(array[i] > maxArray)
+    {
+        maxArray = array[i];
+    }
+    i++;
+}
+Console.Write(maxArray);
